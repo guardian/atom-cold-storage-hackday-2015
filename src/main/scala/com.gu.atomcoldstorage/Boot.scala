142fe870552ed79e6783932b5c9e968f13b72e73
Paul Roberts <paul.roberts@guardian.co.uk>
2015-11-17 13:07:57 +0000

    add sbt-assembly for easy deploy

--- src/main/scala/com.gu.atomcoldstorage/Boot.scala
+++ src/main/scala/com.gu.atomcoldstorage/Boot.scala
@@ -9,14 +9,22 @@ import scala.concurrent.duration._
 
 object Boot extends App {
 
-  // we need an ActorSystem to host our application in
-  implicit val system = ActorSystem("content-atom-cold-storage")
+  val startWebServer = args.exists(_ == "web")
+  val startReader    = args.exists(_ == "reader")
 
-  // create and start our service actor
-  val service = system.actorOf(Props[ColdStorageServiceActor], "cold-storage-service")
+  val port = Option(System.getProperty("coldstorage.port")).map(_.toInt).getOrElse(8080)
 
-  implicit val timeout = Timeout(5.seconds)
-  // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  if(startWebServer) {
 
+    // we need an ActorSystem to host our application in
+    implicit val system = ActorSystem("content-atom-cold-storage")
+
+    // create and start our service actor
+    val service = system.actorOf(Props[ColdStorageServiceActor], "cold-storage-service")
+
+    implicit val timeout = Timeout(5.seconds)
+    // start a new HTTP server on port 8080 with our service actor as the handler
+    IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = port)
+
+  }
 }
